78fbb655b4c481769397dbd3f1f78a426c86966f
Milo McCarty <milomccarty99@gmail.com>
2024-10-15 03:17:16 -0700

    Initial commit

---
+++ Mapify.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuskersClone
+{
+    internal class Mapify
+    {
+        protected int length;
+        protected int width;
+
+        public Mapify(int length, int width) // update with areas player can move/interact with
+        {
+
+        }
+
+        
+    }
+}
